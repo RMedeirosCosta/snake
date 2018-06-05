3e635db800d218b08144c067cd856013c9c5525f
Ricardo Medeiros <ricardo.junior@agrotis.com>
2018-06-05 17:12:25 -0300

    feat: add function draw_reactangle

--- src/draw.rs
+++ src/draw.rs
@@ -18,3 +18,29 @@ pub fn draw_block(color: Color, x: i32, y: i32, con: &Context, g: &mut G2d) {
       g,
   );
 }
+
+pub fn draw_rectangle(
+  color: Color,
+  x: i32,
+  y: i32,
+  width: i32,
+  height: i32,
+  con: &Context,
+  g: &mut G2d
+) {
+  let x = to_coord(x);
+  let y = to_coord(y);
+  
+  rectangle(
+    color,
+    [
+      x,
+      y,
+      BLOCK_SIZE * (width as f64),
+      BLOCK_SIZE * (height as f64),
+    ],
+    con.transform,
+    g,
+  );
+}
+
